9eb13cc30c9b81da62bb6554c19ae84d052bd38c
gwongihong <116726285+rlehd@users.noreply.github.com>
2026-04-29 20:38:24 +0900

    Fix formatting for delete and edit URL paths

--- myDjango/myproject/main/urls.py
+++ myDjango/myproject/main/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_word, name='add_word'),
-    path('delete/<int:pk>/', views.delete_word, name='delete_word'), # 삭제 기능 주소
-    path('edit/<int:pk>/', views.edit_word, name='edit_word'),# 수정 기능
+    path('delete/<int:pk>/', views.delete_word, name='delete_word'), 
+    path('edit/<int:pk>/', views.edit_word, name='edit_word'),
     path('export/', views.export_csv, name = 'export_csv'),
 
 ]
